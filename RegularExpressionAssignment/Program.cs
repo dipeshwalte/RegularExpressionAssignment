cf6cd68e8fd64ee7bef111c352ea24104b4d315b
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-06 10:03:24 +0530

    Add project files.

---
+++ RegularExpressionAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNamePattern = "[A-Z][a-z]{3,}";
+            string[] firstNameInputs = { "dipesh","Dipesh","Di","Dinesh" };
+            Regex re = new Regex(firstNamePattern);
+            foreach (string input in firstNameInputs)
+            {
+                if (re.IsMatch(input))
+                    Console.WriteLine($"{input} is Valid");
+                else
+                    Console.WriteLine($"{input} is inValid");
+            }
+        }
+    }
+}
